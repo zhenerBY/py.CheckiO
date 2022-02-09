3db6c8118b94911877803cd7190aadbd4434915f
zhenerBY <zhener@tut.by>
2022-02-09 12:08:57 +0300

    2

--- Incinerator/Geometry Figures/mission.py
+++ Incinerator/Geometry Figures/mission.py
@@ -1,29 +1,117 @@
+PI = 3.1415926539
+
+
 class Parameters:
-    pass
+
+    def __init__(self, param):
+        self.param = param
+        self.figure = None
+
+    def choose_figure(self, figure):
+        self.figure = figure
+
+    def perimeter(self):
+        return self.output(self.figure.perimeter(self.param))
+
+    def area(self):
+        return self.output(self.figure.area(self.param))
+
+    def volume(self):
+        return self.output(self.figure.volume(self.param))
+
+    @staticmethod
+    def output(param):
+        return param if param == int(param) else round(param, 2)
 
 
 class Circle:
-    pass
+
+    @staticmethod
+    def perimeter(param):
+        return 2 * param * PI
+
+    @staticmethod
+    def area(param):
+        return param ** 2 * PI
+
+    @staticmethod
+    def volume(param):
+        return 0
 
 
 class Triangle:
-    pass
+
+    @staticmethod
+    def perimeter(param):
+        return param * 3
+
+    @staticmethod
+    def area(param):
+        return (3 ** 0.5 / 4) * param * param
+
+    @staticmethod
+    def volume(param):
+        return 0
 
 
 class Square:
-    pass
+
+    @staticmethod
+    def perimeter(param):
+        return param * 4
+
+    @staticmethod
+    def area(param):
+        return param * param
+
+    @staticmethod
+    def volume(param):
+        return 0
 
 
 class Pentagon:
-    pass
+
+    @staticmethod
+    def perimeter(param):
+        return param * 5
+
+    @staticmethod
+    def area(param):
+        return (5 ** 0.5) * ((5 + 2 * 5 ** 0.5) ** 0.5) / 4 * param ** 2
+
+    @staticmethod
+    def volume(param):
+        return 0
 
 
 class Hexagon:
-    pass
+
+    @staticmethod
+    def perimeter(param):
+        return param * 6
+
+    @staticmethod
+    def area(param):
+        return param * param * (3 * 3 ** 0.5) / 2
+
+    @staticmethod
+    def volume(param):
+        return 0
 
 
 class Cube:
-    pass
+
+    @staticmethod
+    def perimeter(param):
+        return param * 12
+
+    @staticmethod
+    def area(param):
+        return param * param * 6
+
+    @staticmethod
+    def volume(param):
+        return param ** 3
 
 
 if __name__ == '__main__':
